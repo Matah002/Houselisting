ac86cea027aee7a82323f4a8b6d83ae90e730d39
Matah002 <jameskimata397@gmail.com>
2024-06-24 18:51:42 +0300

    navigating the admin site

--- housesApp/models.py
+++ housesApp/models.py
@@ -10,8 +10,8 @@ class houselistingModel(models.Model):
     address = models.CharField(max_length=255)
     image = models.ImageField(upload_to='uploads/')
 
-    # def __str__(self):
-    #     return self.title
+    def __str__(self):
+       return self.title
 
 
 
