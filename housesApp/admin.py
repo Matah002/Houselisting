b4a5679a428bf09106dceabedc64b1ef0198150f
Matah002 <jameskimata397@gmail.com>
2024-06-24 18:27:03 +0300

    creating superuser

--- housesApp/admin.py
+++ housesApp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+# from the housesApp, and in the models file, import the class houselistingModel
+from .models import houselistingModel 
 
 # Register your models here.
+admin.site.register(houselistingModel)
--- housesApp/models.py
+++ housesApp/models.py
@@ -2,10 +2,16 @@ from django.db import models
 
 # Create your models here.
 class houselistingModel(models.Model):
-    # The attribute
+    # The attribute/fields/table/colums
     title = models.CharField(max_length=255)
     price = models.IntegerField()
     square_feet = models.IntegerField()
     no_of_bedrooms = models.IntegerField()
     address = models.CharField(max_length=255)
-    image = models.ImageField()
+    image = models.ImageField(upload_to='uploads/')
+
+    # def __str__(self):
+    #     return self.title
+
+
+
